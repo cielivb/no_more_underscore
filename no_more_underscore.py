c45f7e48f5a99859750da0fdb7dd83843b33b3d3
Ciel Baumann <ciel.baumann@gmail.com>
2025-11-21 16:05:05 +1300

    Scaffold GUI

--- no_more_underscore.py
+++ no_more_underscore.py
@@ -11,6 +11,20 @@ e.g., 1987_dwyer_et_al_meaning_of_pluto.docx will be printed as
    
 """
 
+from tkinter import Tk
+from tkinter import Menu
+from tkinter import Label
+from tkinter import Button
+from tkinter import Text
+from tkinter import Frame
+from tkinter import E
+from tkinter import W
+from tkinter import scrolledtext
+
+
+
+ROOT = None
+
 
 ### GUI Logic ---------------------------------------------------------------
 
@@ -21,7 +35,44 @@ e.g., 1987_dwyer_et_al_meaning_of_pluto.docx will be printed as
 
 ### GUI Structure -----------------------------------------------------------
 
+def create_header():
+    global ROOT
+    header = Frame()
+    Label(header, text='Select directory:').grid(row=0, column=0, sticky=W)
+    Button(header, text='Select', command=None).grid(row=0, column=1, sticky=E, padx=20)
+    return header
 
+
+def create_main_window():
+    # Create root window
+    global ROOT
+    ROOT = Tk()
+    ROOT.title("no more underscore")
+    ROOT.geometry('500x500') # width x height of root window
+    
+    # Create menu bar
+    menu_bar = Menu(ROOT)
+    menu_bar.add_cascade(label='About', command=None)
+    menu_bar.add_cascade(label='License', command=None) # COMMAND TO DO
+    ROOT.config(menu=menu_bar)
+    
+    # Configure column and row widths
+    ROOT.columnconfigure(0, weight=1)
+    ROOT.rowconfigure(0, weight=1, pad=1)
+    ROOT.rowconfigure(1, weight=1, pad=1)
+    ROOT.rowconfigure(2, weight=1, pad=1)
+    
+    # Create header section
+    header = create_header()
+    header.grid(row=0, column=0)
+    
+    # Create output section
+    out_field = scrolledtext.ScrolledText(ROOT, width=50, height=25)
+    dir_label = Label(ROOT, text='This is the directory label')
+    out_field.grid(row=1, column=0)
+    dir_label.grid(row=2, column=0)
+    
+    ROOT.mainloop()
 
 
 
